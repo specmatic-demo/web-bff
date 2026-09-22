a2bee369effdb8f2dd588fc13e694d93f041f42e
jaydeep <jaydeepk@gmail.com>
2026-09-22 11:36:46 +0530

    fix: set provider identity for central reports

--- ci_central_repo_report.sh
+++ ci_central_repo_report.sh
@@ -8,6 +8,15 @@ source "${SCRIPT_DIR}/_ci-common.sh"
 init_specmatic_cmd
 init_colors
 
+REPO_URL="$(git -C "${SCRIPT_DIR}" config --get remote.origin.url | sed -E 's#^git@github.com:#https://github.com/#; s#\.git$##')"
+REPO_SLUG="${REPO_URL#https://github.com/}"
+export SPECMATIC_REPO_ID="$(gh api "repos/${REPO_SLUG}" --jq .id)"
+export SPECMATIC_REPO_NAME="${REPO_SLUG##*/}"
+export SPECMATIC_REPO_URL="${REPO_URL}"
+export SPECMATIC_BUILD_ID="${SPECMATIC_BUILD_ID:-${GITHUB_RUN_ID:-local-central-report}}"
+export SPECMATIC_BRANCH_NAME="${GITHUB_HEAD_REF:-${GITHUB_REF_NAME:-main}}"
+export SPECMATIC_RUN_ATTEMPT="${GITHUB_RUN_ATTEMPT:-1}"
+
 SPECS_DIR="${SCRIPT_DIR}/specs"
 
 if [[ ! -d "${SPECS_DIR}" ]]; then
