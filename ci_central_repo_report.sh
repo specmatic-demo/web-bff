d288c449042368601a2372f302dda39012fe7ddc
jaydeep <jaydeepk@gmail.com>
2026-09-22 10:32:06 +0530

    chore: remove manual report upload

--- ci_central_repo_report.sh
+++ ci_central_repo_report.sh
@@ -19,14 +19,3 @@ cd "${SPECS_DIR}"
 
 echo "${C_BLUE}Generating central contract repo report from ${SPECS_DIR}${C_RESET}"
 "${SPECMATIC_CMD[@]}" central-contract-repo-report 2>&1 | prefix_output "$C_GREEN" "central-repo-report"
-
-if [[ -z "${SEND_REPORT:-}" ]]; then
-  exit 0
-fi
-
-echo "${C_BLUE}Sending central contract repo report to Insights from ${SPECS_DIR}${C_RESET}"
-"${SPECMATIC_CMD[@]}" send-report \
-  --repo-id="$(gh api 'repos/{owner}/{repo}' --jq .id)" \
-  --repo-name="$(gh repo view --json name -q .name)" \
-  --repo-url="$(gh repo view --json url --jq .url)" \
-  --branch-name main
